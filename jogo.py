f92318b629cf1433cad3432a165dacb02870d450
elcineyjr <elcineyjr@gmail.com>
2022-02-17 19:43:14 -0300

    :sparkles: Add complete match setup with sets and games

--- jogo.py
+++ jogo.py
@@ -1,78 +1,121 @@
 import random
 
-class Jogo:
-  def __init__(self, probabilidadeA):
-    self.probabilidadeA = probabilidadeA
-    self.pontos = ['0','0']
-    self.vencedor = None
-    self.set = [0,0]
-  
-  def joga(self):
-      while 2 not in self.set:
-          self.joga_set()
-          if self.vencedor == 'A':
-              self.set[0] = self.set[0]+1
-          else:
-              self.set[1] = self.set[1]+1
-      return self.vencedor,self.pontos
+class Game:
+    def __init__(self, probabilidadeA):
+        self.probabilidadeA = probabilidadeA
+        self.pontos = {
+            "A": "0",
+            "B": "0"
+        }
+        self.vencedor = None
 
-  def joga_set(self):
+    def joga_game(self):
       while self.vencedor == None:
           self.joga_round()
 
-  def joga_round(self):
-      resultado = random.uniform(0, 1)
-      if resultado <= self.probabilidadeA:
-          self.ganha_A()
-      else:
-          self.ganha_B()
+    def joga_round(self):
+        resultado = random.uniform(0, 1)
+
+        if resultado <= self.probabilidadeA:
+            self.ganha_A()
+        else:
+            self.ganha_B()
   
-  def ganha_A(self):
-      if self.pontos[0] == '0':
-          self.pontos[0] = '15'
-          self.pontos[1] = 'Love'
-      elif self.pontos[0] == '15' and self.pontos[1] != '30' and self.pontos[1] != '40':
-          self.pontos[0] = '30'
-      elif self.pontos[0] == '30':
-          self.pontos[0] = '40'
-      elif self.pontos[0] == '40' or self.pontos[0] == 'Adv A':
-          self.pontos[0] = 'G'
-          self.vencedor = 'A'
-      elif self.pontos[0] == 'Love':
-          self.pontos[0] = '15'
-      elif (self.pontos[0] == '15' and self.pontos[1] == '30') or self.pontos[0] == 'Adv B':
-          self.pontos[0] = 'Deuce'
-          self.pontos[1] = 'Deuce'
-      elif self.pontos[0] == '15' and self.pontos[1] == '40':
-          self.pontos[0] = 'Adv B'
-          self.pontos[1] = 'Adv B'
-      elif self.pontos[0] == 'Deuce':
-          self.pontos[0] = 'Adv A'
-          self.pontos[1] = 'Adv A'
+    def ganha_A(self):
+        if self.pontos["A"] == '0':
+            self.pontos["A"] = '15'
+            self.pontos["B"] = 'Love'
+        elif self.pontos["A"] == '15' and self.pontos["B"] != '30' and self.pontos["B"] != '40':
+            self.pontos["A"] = '30'
+        elif self.pontos["A"] == '30':
+            self.pontos["A"] = '40'
+        elif self.pontos["A"] == '40' or self.pontos["A"] == 'Adv A':
+            self.pontos["A"] = 'G'
+            self.vencedor = 'A'
+        elif self.pontos["A"] == 'Love':
+            self.pontos["A"] = '15'
+        elif (self.pontos["A"] == '15' and self.pontos["B"] == '30') or self.pontos["A"] == 'Adv B':
+            self.pontos["A"] = 'Deuce'
+            self.pontos["B"] = 'Deuce'
+        elif self.pontos["A"] == '15' and self.pontos["B"] == '40':
+            self.pontos["A"] = 'Adv B'
+            self.pontos["B"] = 'Adv B'
+        elif self.pontos["A"] == 'Deuce':
+            self.pontos["A"] = 'Adv A'
+            self.pontos["B"] = 'Adv A'
 
 
-  def ganha_B(self):
-      if self.pontos[1] == '0':
-          self.pontos[1] = '15'
-          self.pontos[0] = 'Love'
-      elif self.pontos[1] == '15' and self.pontos[0] != '30' and self.pontos[0] != '40':
-          self.pontos[1] = '30'
-      elif self.pontos[1] == '30':
-          self.pontos[1] = '40'
-      elif self.pontos[1] == '40' or self.pontos[1] == 'Adv A':
-          self.pontos[1] = 'G'
-          self.vencedor = 'B'
-      elif self.pontos[1] == 'Love':
-          self.pontos[1] = '15'
-      elif (self.pontos[1] == '15' and self.pontos[0] == '30') or self.pontos[0] == 'Adv B':
-          self.pontos[0] = 'Deuce'
-          self.pontos[1] = 'Deuce'
-      elif self.pontos[1] == '15' and self.pontos[0] == '40':
-          self.pontos[0] = 'Adv A'
-          self.pontos[1] = 'Adv A'
-      elif self.pontos[1] == 'Deuce':
-          self.pontos[0] = 'Adv B'
-          self.pontos[1] = 'Adv B'
-  
+    def ganha_B(self):
+        if self.pontos["B"] == '0':
+            self.pontos["B"] = '15'
+            self.pontos["A"] = 'Love'
+        elif self.pontos["B"] == '15' and self.pontos["A"] != '30' and self.pontos["A"] != '40':
+            self.pontos["B"] = '30'
+        elif self.pontos["B"] == '30':
+            self.pontos["B"] = '40'
+        elif self.pontos["B"] == '40' or self.pontos["B"] == 'Adv A':
+            self.pontos["B"] = 'G'
+            self.vencedor = 'B'
+        elif self.pontos["B"] == 'Love':
+            self.pontos["B"] = '15'
+        elif (self.pontos["B"] == '15' and self.pontos["A"] == '30') or self.pontos["A"] == 'Adv B':
+            self.pontos["A"] = 'Deuce'
+            self.pontos["B"] = 'Deuce'
+        elif self.pontos["B"] == '15' and self.pontos["A"] == '40':
+            self.pontos["A"] = 'Adv A'
+            self.pontos["B"] = 'Adv A'
+        elif self.pontos["B"] == 'Deuce':
+            self.pontos["A"] = 'Adv B'
+            self.pontos["B"] = 'Adv B'
 
 
+
+class Set:
+    def __init__(self, probabilidadeA):
+        self.probabilidadeA = probabilidadeA
+        self.pontos = {
+            "A": 0,
+            "B": 0
+        }
+        self.gamesJogados = 0
+        self.vencedor = None
+    
+    def joga_set(self):
+        while self.gamesJogados < 7:
+            game = Game(self.probabilidadeA)
+
+            game.joga_game()
+
+            self.pontos[game.vencedor] += 1            
+            self.gamesJogados += 1
+        
+        # Define o vencedor do set
+        if self.pontos["A"] > self.pontos["B"]:
+            self.vencedor = "A"
+        else: 
+            self.vencedor = "B"
+
+class Partida:
+    def __init__(self, probabilidadeA):
+        self.probabilidadeA = probabilidadeA
+        self.setsJogados = 0
+        self.setsGanhos = {
+            "A": 0,
+            "B": 0
+        }
+        self.vencedor = None
+    
+    def joga_partida(self):
+        while self.setsJogados < 3:
+            set = Set(self.probabilidadeA)
+            
+            set.joga_set()
+
+            self.setsGanhos[set.vencedor] += 1
+            self.setsJogados += 1
+
+        # Define o vencedor da partida
+        if self.setsGanhos["A"] > self.setsGanhos["B"]:
+            self.vencedor = "A"
+        else: 
+            self.vencedor = "B"
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from jogo import Jogo
+from jogo import Partida
+
+# 1 partida = melhor de 3 sets
+# 1 set = melhor de 7 games
+# 1 game = cadeia de markov
+
 def main():
-    j = Jogo(0.5)
-    vencedor,resultado = j.joga()
-    print(vencedor)
-    print(resultado)
+    j = Partida(0.5)
+    j.joga_partida()
+
+    print(j.vencedor)
+    print(j.setsGanhos)
 
 if __name__ == "__main__":
     main()
