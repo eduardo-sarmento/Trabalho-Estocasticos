37e3b86d4f015f59e05d49959d4ec5fba4775015
faakit <64740422+faakit@users.noreply.github.com>
2022-02-25 17:42:19 -0300

    Game logics fixed

--- jogo.py
+++ jogo.py
@@ -1,4 +1,5 @@
 import random
+from utils import moduloDe
 
 class Game:
     def __init__(self, probabilidadeA):
@@ -72,7 +73,6 @@ class Game:
             self.pontos["B"] = 'Adv B'
 
 
-
 class Set:
     def __init__(self, probabilidadeA):
         self.probabilidadeA = probabilidadeA
@@ -84,15 +84,19 @@ class Set:
         self.vencedor = None
     
     def joga_set(self):
-        while self.gamesJogados < 7:
+        # Para vencer o set ou algum chega a 6 pontos com 2+ de diferença ou são jogados 13 jogos
+        while (((moduloDe(self.pontos['A'] - self.pontos['B'])>=2) & ((self.pontos['A'] == 6) | (self.pontos['B'] == 6))) == False) & (self.gamesJogados < 13)  :
             game = Game(self.probabilidadeA)
 
             print(f"\nGame {self.gamesJogados}:")
             game.joga_game()
 
+            print(self.gamesJogados < 14)
+
             self.pontos[game.vencedor] += 1            
             self.gamesJogados += 1
         
+        print(f"\nA: {self.pontos['A']} || B: {self.pontos['B']}")
         # Define o vencedor do set
         if self.pontos["A"] > self.pontos["B"]:
             self.vencedor = "A"
@@ -110,14 +114,14 @@ class Partida:
         self.vencedor = None
     
     def joga_partida(self):
-        while self.setsJogados < 3:
+        while ((self.setsGanhos["A"] != 2) & (self.setsGanhos["B"] != 2)):
             set = Set(self.probabilidadeA)
             
-            print(f"Iniciando Set {self.setsJogados}:")
+            print(f"Iniciando Set {self.setsJogados+1}:")
 
             set.joga_set()
 
-            print(f"\n=== Vencedor do set {self.setsJogados}: {set.vencedor}\n")
+            print(f"\n=== Vencedor do set {self.setsJogados+1}: {set.vencedor}\n")
 
             self.setsGanhos[set.vencedor] += 1
             self.setsJogados += 1
---
+++ utils.py
@@ -0,0 +1,6 @@
+#Retorna o módulo do inteiro
+def moduloDe(numero):
+    if numero > 0:
+        return numero
+    else:
+        return -numero
